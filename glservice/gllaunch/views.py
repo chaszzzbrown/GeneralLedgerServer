4588577e6344cc46aaf97475084bc7ce7ec0460b
Chas Brown <charlieb@redhillstudios.com>
2013-06-11 18:32:16 -0700

    Initial commit

---
+++ glservice/gllaunch/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.template import Context, Template
+from django.template.loader import get_template
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
+def echo_LTI_vars(request):
+    s = "<html><body>"
+    s += "<h1>List of request params</h1>"
+    for k,v in request.REQUEST.items():
+        s += "<p>"+k+":"+v+"</p>"
+    s+="</body></html>"
+    return HttpResponse(s)
---
+++ glservice/glservice/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from gllaunch.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^gllaunch/echo_test/', echo_LTI_vars),
+    # Examples:
+    # url(r'^$', 'glservice.views.home', name='home'),
+    # url(r'^glservice/', include('glservice.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
