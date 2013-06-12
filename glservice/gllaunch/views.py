ed3c6e5d9da3fff30e7434d7ec11c94fccd40ed4
Chas Brown <charlieb@redhillstudios.com>
2013-06-11 19:38:38 -0700

    DIstinguish between POST and GET params

--- glservice/gllaunch/views.py
+++ glservice/gllaunch/views.py
@@ -9,8 +9,13 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def echo_LTI_vars(request):
     s = "<html><body>"
-    s += "<h1>List of request params</h1>"
-    for k,v in request.REQUEST.items():
-        s += "<p>"+k+":"+v+"</p>"
+    s += "<h1>List of POST request params</h1><ul>"
+    for k,v in request.POST.items():
+        s += "<li>"+k+":"+v+"</li>"
+    s += "</ul>"
+    s += "<h1>List of GET request params</h1><ul>"
+    for k,v in request.GET.items():
+        s += "<li>"+k+":"+v+"<//li>"
+    s += "</ul>"
     s+="</body></html>"
     return HttpResponse(s)
